a66b79da5109825a3f66567ea67fa217208ccf4c
supermetrolog <billypro6@gmail.com>
2022-03-30 00:25:22 +0300

    add droped favorites list in header

--- src/components/common/layouts/main/Header.vue
+++ src/components/common/layouts/main/Header.vue
@@ -1,10 +1,26 @@
 <template>
-  <header class="row no-gutters">
-    <div class="header__menu col-4"><i class="fas fa-bars"></i></div>
-    <div class="header__logo col-4 text-center">
-      <div class="image-container">
-        <img src="../../../../assets/images/logo.png" alt="" />
+  <header>
+    <div class="header row no-gutters">
+      <div class="header__menu col-4"><i class="fas fa-bars"></i></div>
+      <div class="header__logo col-4 text-center">
+        <div class="image-container">
+          <img src="../../../../assets/images/logo.png" alt="" />
+        </div>
       </div>
+      <div class="col-4 header__favorites text-right">
+        <i class="fas fa-bookmark" @click="onClickFavorites">
+          <span class="badge badge-danger">2</span>
+        </i>
+      </div>
+      <transition
+        mode="out-in"
+        enter-active-class="animate__animated animate__fadeInDown for__notifications"
+        leave-active-class="animate__animated animate__fadeOutUp for__notifications"
+      >
+        <div class="header__droped-favorites-list" v-if="favoritesVisible">
+          favorites
+        </div>
+      </transition>
     </div>
   </header>
 </template>
@@ -12,6 +28,16 @@
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      favoritesVisible: false,
+    };
+  },
+  methods: {
+    onClickFavorites() {
+      this.favoritesVisible = !this.favoritesVisible;
+    },
+  },
 };
 </script>
 
