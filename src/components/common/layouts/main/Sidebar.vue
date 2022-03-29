3b14d27dff712dd3df44d184735ed58184b1cbce
supermetrolog <billypro6@gmail.com>
2022-03-29 23:36:02 +0300

    developed sidebar

--- src/components/common/layouts/main/Sidebar.vue
+++ src/components/common/layouts/main/Sidebar.vue
@@ -1,14 +1,24 @@
 <template>
   <section class="sidebar">
     <ul class="categories">
-      <li>
-        <div class="image-container">
-          <img src="../../../../assets/images/logo.png" alt="" />
+      <li
+        v-for="category of categories"
+        :key="category.name"
+        :title="category.title"
+        @click="onClickCategory(category)"
+        :class="{ active: $route.query.category == category.name }"
+      >
+        <div class="categories__image">
+          <div class="image-container">
+            <img
+              src="../../../../assets/images/logo.png"
+              :alt="category.title"
+            />
+          </div>
         </div>
-      </li>
-      <li>
-        <div class="image-container">
-          <img src="../../../../assets/images/logo.png" alt="" />
+
+        <div class="categories__title">
+          <p>{{ category.title }}</p>
         </div>
       </li>
     </ul>
@@ -18,6 +28,47 @@
 <script>
 export default {
   name: "Sidebar",
+  data() {
+    return {
+      categories: [
+        {
+          title: "кальяны",
+          image: "fuck.jpg",
+          name: "hookah",
+        },
+        {
+          title: "кухня",
+          image: "fuck.jpg",
+          name: "kitchen",
+        },
+        {
+          title: "алкоголь",
+          image: "fuck.jpg",
+          name: "alcohol",
+        },
+        {
+          title: "напитки",
+          image: "fuck.jpg",
+          name: "drink",
+        },
+        {
+          title: "чай",
+          image: "fuck.jpg",
+          name: "tea",
+        },
+      ],
+    };
+  },
+  methods: {
+    onClickCategory(category) {
+      let query = {
+        category: category.name,
+      };
+      this.$router.replace({
+        query,
+      });
+    },
+  },
 };
 </script>
 
