9f7726aeac033f2eea7aab4631471b09ebd1024a
supermetrolog <billypro6@gmail.com>
2022-04-19 01:32:06 +0300

    add to sidebar current sub category

--- src/components/common/layouts/main/Sidebar.vue
+++ src/components/common/layouts/main/Sidebar.vue
@@ -19,6 +19,11 @@
         </div>
       </li>
     </ul>
+    <div class="sub-category-list">
+      <div class="current-sub-category" v-if="currentSubCategory">
+        <p>{{ currentSubCategory.title }}<i class="fas fa-angle-down"></i></p>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -28,13 +33,27 @@ import { mapActions, mapGetters } from "vuex";
 export default {
   name: "Sidebar",
   computed: {
-    ...mapGetters(["CATEGORIES"]),
+    ...mapGetters(["CATEGORIES", "DATA"]),
     imageSrc() {
       if (process.env.NODE_ENV == "development") {
         return "http://menu/images/icon.png";
       } else {
         return "https://myatatasty.store/images/icon.png";
       }
+    },
+    currentSubCategory() {
+      if (!this.DATA || !this.DATA.length) {
+        return null;
+      }
+      let currentSubCategory = null;
+      this.DATA.forEach((category) => {
+        category.subCategories.forEach((sub_category) => {
+          if (sub_category.id == this.$route.query.sub_category) {
+            currentSubCategory = sub_category;
+          }
+        });
+      });
+      return currentSubCategory;
     },
   },
   methods: {
