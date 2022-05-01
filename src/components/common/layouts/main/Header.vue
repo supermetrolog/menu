8582bce0473d5c5b2c0b008fef6b74f7550ac81a
supermetrolog <billypro6@gmail.com>
2022-05-02 00:57:24 +0300

    fix bug

--- src/components/common/layouts/main/Header.vue
+++ src/components/common/layouts/main/Header.vue
@@ -32,7 +32,7 @@ export default {
       if (process.env.NODE_ENV == "development") {
         return "http://menu/images/header_logo2.png";
       } else {
-        return "https://myatatasty.store/images/header_logo.png";
+        return "https://myatatasty.store/images/header_logo2.png";
       }
     },
   },
